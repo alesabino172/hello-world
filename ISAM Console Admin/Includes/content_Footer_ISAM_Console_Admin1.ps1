15b8a20e0a69739bc34a44f65abecb5a4b293618
Alessandro Sabino <54832366+alesabino172@users.noreply.github.com>
2022-07-02 18:16:58 -0300

    Create content_Footer_ISAM_Console_Admin1.ps1

---
+++ ISAM Console Admin/Includes/content_Footer_ISAM_Console_Admin1.ps1
@@ -0,0 +1,13 @@
+
+footer -Class "footer-admin mt-auto footer-light" {
+    div -Class "container-xl px-4" {
+        div -Class "row" {
+            div -Class "col-md-6 small" { "Direitos autorais © Seu site 2021" }
+            div -Class "col-md-6 text-md-end small" {
+                a -href "dashboard-1.html#!" { "Política de Privacidade" }
+                "·"
+                a -href "dashboard-1.html#!" { "Termos &amp; Condições" }
+            }
+        }
+    }
+}
